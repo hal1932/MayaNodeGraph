23fbe1ad19db12cf3497e0db8543acb5c2326689
hal1932 <yu.arai.19@gmail.com>
2019-03-17 23:03:09 +0900

    use cmds

--- MayaNodeGraph/Program.cs
+++ MayaNodeGraph/Program.cs
@@ -10,9 +10,26 @@ namespace MayaNodeGraph
         {
             var filePath = @"C:\Program Files\Autodesk\Maya2018\Examples\Animation\Rigs\rp_janna_rigged_HIK_arnold.ma";
 
+            var graph = default(NodeGraph);
             using (var reader = new SceneFileReader(filePath))
             {
-                var graph = await NodeGraph.LoadAsync(reader);
+                graph = await NodeGraph.LoadAsync(reader);
+            }
+
+            var cmds = new CommandProxy(graph);
+            foreach (var node in cmds.Ls(typeName: "transform"))
+            {
+                Console.WriteLine(node.Name);
+            }
+            Console.WriteLine("=====================");
+            foreach (var node in cmds.ListConnections("root"))
+            {
+                Console.WriteLine(node.Name);
+            }
+            Console.WriteLine("=====================");
+            foreach (var node in cmds.ListRelatives("root"))
+            {
+                Console.WriteLine(node.Name);
             }
 
             Console.ReadKey();
